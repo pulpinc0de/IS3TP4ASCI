29407d6d585db7be231355c53b033865d96ae7aa
Carolina Munz <carolinamunz43@gmail.com>
2025-07-10 23:32:44 -0300

    add unit test for multiplicacion

--- test/calculos.py
+++ test/calculos.py
@@ -1,11 +1,3 @@
-def suma(a, b):
-    try:
-        num_a = int(a)
-        num_b = int(b)
-        return num_a + num_b
-    except (TypeError, ValueError):
-        return  None
-
 def multiplicacion(a, b):
     try:
         num_a = int(a)
--- test/test_calculos.py
+++ test/test_calculos.py
@@ -1,19 +1,20 @@
-from calculos import suma
+from .calculos import multiplicacion
 import pytest
+
 
 @pytest.mark.parametrize(
     "valor_a, valor_b, resultado",
     [
-        (2, 3, 5),           
-        (2, -3, -1),           
-        (0, 5, 5),
+        (2, 3, 6),           
+        (2, -3, -6),           
+        (0, 5, 0),
         (None, 5, None),     
         (None, None, None), 
         (2, None, None),    
-        ('2', 5, 7),
-        (2, suma(2, 2), 6),
+        ('2', 5, 10),
+        (2, multiplicacion(2, 2), 8),
     ]
 )
-def test_varios_casos(valor_a, valor_b, resultado):
-    assert suma(valor_a, valor_b) == resultado
+def test_varios_casos_multiplicacion(valor_a, valor_b, resultado):
+    assert multiplicacion(valor_a, valor_b) == resultado
 
